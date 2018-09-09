c72133af2fd565c0de462757ef28cf9b77360039
nehalist <github@nehalist.io>
2018-09-09 16:17:17 +0200

    Add more header styling

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,12 +1,25 @@
 $(() => {
-  const adjustNavbarBgColor = () => {
-    const scrollTop = $('html').scrollTop();
-    const header    = $('.header');
-    const alpha     = scrollTop / header.height();
+  const $document      = $(document);
+  const $navigation    = $('.navbar');
+  const $header        = $('.header');
+  const $logo          = $('.header__logo');
+  const $navLogo       = $('.navigation__logo');
+  const $logoContainer = $('.navigation__logo-container');
 
-    $('.navbar').css('background-color', `rgba(52, 58, 64, ${alpha}`);
+  const adjustNavigationBgColor = () => {
+    const scrollTop = $document.scrollTop();
+    const alpha     = scrollTop / $header.height();
+
+    $navigation.css('background-color', `rgba(52, 58, 64, ${alpha}`);
   };
 
-  $(document).on('scroll', adjustNavbarBgColor);
-  $(document).on('load', adjustNavbarBgColor);
+  const logoSwitcher = () => {
+    const scrollTop = $document.scrollTop();
+    const padding = (scrollTop > $logo.offset().top + $logo.height()) ? 0 : '30px';
+    $logoContainer.css('padding-top', padding);
+  };
+
+  $document.on('scroll load', logoSwitcher);
+
+  $document.on('scroll load', adjustNavigationBgColor);
 });
