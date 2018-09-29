a987f288d335293d4e90baaaad27c82fb8ab70ab
nehalist <github@nehalist.io>
2018-09-29 13:14:51 +0200

    Add basic search functionality

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -2,6 +2,10 @@ $(() => {
   const $document = $(document);
   const $logo = $('.header__logo');
   const $logoContainer = $('.navigation__logo-container');
+  const $sidebar = $('.sidebar');
+  const $window = $(window);
+  const $navigation = $('.navigation');
+  const $search = $('.search');
 
   const logoSwitcher = () => {
     const scrollTop = $document.scrollTop();
@@ -12,14 +16,23 @@ $(() => {
   const adjustNavigationBgColor = () => {
     const scrollTop = $document.scrollTop();
     const alpha     = scrollTop / $('.header').height();
-
-    $('.navigation').css('background-color', `rgba(9, 10, 11, ${alpha})`);
+    $navigation.css('background-color', `rgba(9, 10, 11, ${alpha})`);
   };
 
   if ($logo.length) {
     $document.on('scroll', logoSwitcher);
     logoSwitcher();
   }
+
+  if ($sidebar.height() < ($window.height() - $navigation.height())) {
+
+  }
+
+  $('.toggle-search').on('click', () => {
+    $('body').css('overflow-y', $search.is(':visible') ? 'auto' : 'hidden');
+    $search.css('top', $document.scrollTop() + 56);
+    $search.fadeToggle();
+  });
 
   $document.on('scroll', adjustNavigationBgColor);
   adjustNavigationBgColor();
